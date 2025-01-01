5503d3485c5a1940cb84f399d570bf78f8a63099
YuyaAizawa <selfish.curiosity@gmail.com>
2025-01-01 15:21:46 +0900

    追加 elm-canvasの動作確認

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,13 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text, button)
+import Canvas exposing (Point, shapes, rect)
+import Canvas.Settings exposing (fill)
+import Color
+import Html exposing (Attribute, Html, div)
 import Html.Attributes exposing (class, style)
-import Html.Events exposing (onClick)
+import Html.Events
+import Json.Decode as Json
 
 
 
@@ -16,43 +20,128 @@ main =
 
 
 
+-----------
 -- MODEL --
+-----------
 
 type alias Model =
-  { cnt : Int }
+  { block : Point
+  , grabbed : Maybe Point
+  }
 
 init : Model
 init =
-  { cnt = 3 }
+  { block = ( 10, 10 )
+  , grabbed = Nothing
+  }
+
+blockSize = 20
 
 
 
+------------
 -- UPDATE --
+------------
 
 type Msg
-  = Increment
-  | Decrement
+  = MouseDown MouseState
+  | MouseUp MouseState
+  | MouseMove MouseState
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      { model | cnt = min 5 (model.cnt + 1) }
+    MouseDown mouse ->
+      { model | grabbed = intoBlock model.block ( mouse.x, mouse.y ) }
 
-    Decrement ->
-      { model | cnt = max 1 (model.cnt - 1) }
+    MouseUp mouse ->
+      { model | grabbed = Nothing }
+
+    MouseMove mouse ->
+      case model.grabbed of
+        Nothing ->
+          model
+        Just ( x, y ) ->
+          { model | block = ( mouse.x - x, mouse.y - y ) }
+
+intoBlock : Point -> Point -> Maybe Point
+intoBlock ( blockX, blockY ) ( mouseX, mouseY ) =
+  let
+    x = mouseX - blockX
+    y = mouseY - blockY
+  in
+    if 0 <= x && x < blockSize && 0 <= y && y < blockSize then
+      Just ( x, y )
+    else
+      Nothing
 
 
 
+----------
 -- VIEW --
+----------
+
+canvasWidth = 640
+canvasHeight = 480
+
+fillBackGound =
+  shapes [ fill Color.white ] [ rect (0, 0) canvasWidth canvasHeight ]
+
+renderBlock upperLeft =
+  shapes [ fill Color.red ]
+    [ rect upperLeft blockSize blockSize ]
 
 view : Model -> Html Msg
 view model =
   div [ class "contents" ]
-    [ div []
-        [ button [ onClick Increment ] [ text "👍"]
-        , button [ onClick Decrement ] [ text "👎"]
-        ]
-    , div []
-      [ text <| String.concat <| List.repeat model.cnt "🌟" ]
+    [ Canvas.toHtml (canvasWidth, canvasHeight)
+      [ style "display" "block"
+      , style "border" "1px solid red"
+      , onMouseUp MouseUp
+      , onMouseDown MouseDown
+      , onMouseMove MouseMove
+      ]
+      [ fillBackGound
+      , renderBlock model.block
+      ]
     ]
+
+
+
+-----------
+-- MOUSE --
+-----------
+
+type alias MouseState =
+  { x : Float
+  , y : Float
+  , dx : Float
+  , dy : Float
+  }
+
+mouseDecoder : Json.Decoder MouseState
+mouseDecoder =
+  Json.map4
+    MouseState
+    (Json.field "offsetX" Json.float)
+    (Json.field "offsetY" Json.float)
+    (Json.field "movementX" Json.float)
+    (Json.field "movementY" Json.float)
+
+onMouseDown : (MouseState -> msg) -> Attribute msg
+onMouseDown msgMapper =
+  mouseDecoder
+    |> Json.map msgMapper
+    |> Html.Events.on "mousedown"
+
+onMouseUp : (MouseState -> msg) -> Attribute msg
+onMouseUp msgMapper =
+  mouseDecoder
+    |> Json.map msgMapper
+    |> Html.Events.on "mouseup"
+
+onMouseMove : (MouseState -> msg) -> Attribute msg
+onMouseMove msgMapper =
+  mouseDecoder
+    |> Json.map msgMapper
+    |> Html.Events.on "mousemove"
